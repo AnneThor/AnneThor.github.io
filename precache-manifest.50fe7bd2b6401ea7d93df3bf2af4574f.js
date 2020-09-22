95b88d0f379a1db69ce19ac27ed8bd021f3a63a7
Anne Thor <anne.thor@gmail.com>
2020-09-21 21:55:29 -0700

    Updates

---
+++ precache-manifest.50fe7bd2b6401ea7d93df3bf2af4574f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2462cd4afce02beeff687a82e36a84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cd0c1179a1e2119ea8f",
+    "url": "/static/css/main.3daad53a.chunk.css"
+  },
+  {
+    "revision": "9e1f7252727f769c5829",
+    "url": "/static/js/2.951fb892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.951fb892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd0c1179a1e2119ea8f",
+    "url": "/static/js/main.841da62a.chunk.js"
+  },
+  {
+    "revision": "9e57fcf4a19e04be7557",
+    "url": "/static/js/runtime-main.a0c1249e.js"
+  }
+]);
