2cdbf5ce4518ad1c0ef3e915b08e7e5e0cf3668e
Anne Thor <anne.thor@gmail.com>
2020-09-26 11:12:39 -0700

    input basic resume format, adjusted styling

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -7,34 +7,63 @@ function Resume(props) {
 
     <section id="resume">
       <div className="resume-container">
-        <p className="education">Education:</p>
+
+        <div className="education">
+          <img src="./images/resume/education.png"
+               alt="graduation hat logo"/>
+
+        </div>
         <div className="education-details">
           <p>MA, International Relations</p>
           <p>Jackson School, University of Washington</p>
         </div>
 
-        <p className="work">Work:</p>
+        <div className="work">
+          <img src="./images/resume/work.png"
+               alt="briefcase logo"/>
+
+        </div>
         <div className="work-details">
           <p>Account Manager, MIR Corporation</p>
           <p>International Travel Expert</p>
           <p>Seattle, WA</p>
         </div>
 
-        <p className="stack">Stack:</p>
+        <div className="stack">
+          <img src="./images/resume/stack.png"
+               alt="code on computer screen logo"/>
+        </div>
+          <ul className="stack-1">
+            <li>JavaScript</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Python</li>
+            <li>SQL</li>
+            <li>Java</li>
+            <li>Git</li>
+          </ul>
+          <ul className="stack-2">
+            <li>Bootstrap</li>
+            <li>Redux</li>
+            <li>D3.js</li>
+            <li>React</li>
+            <li>Node</li>
+          </ul>
+          <ul className="stack-3">
+            <li>Web Design</li>
+            <li>Database Design</li>
+            <li>TDD</li>
+            <li>Agile</li>
+            <li>OOD</li>
+          </ul>
 
-        <ul className="stack-details">
-          <li>JavaScript</li><li>HTML5</li><li>CSS3</li>
-          <li>Web Design</li><li>Redux</li><li>D3.js</li>
-          <li>React</li><li>Python</li><li>SQL</li>
-          <li>Database Design</li><li>Java</li><li>TDD</li>
-          <li>Bootstrap</li><li>Git</li><li>Agile</li>
-          <li>OOD</li>
-        </ul>
-
-
-        <p className="skills">Skills:</p>
+        <div className="skills">
+          <img src="./images/resume/skills.png"
+               alt="logo of human juggling tasks"/>
+        </div>
         <div className="skills-details">
           <p>Experienced project manager</p>
+          <p>Client Sales</p>
         </div>
       </div>
     </section>
